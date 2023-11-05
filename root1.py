b24efa368623fb99c55ef263b0c2e59ff88d1aee
Bishakha143 <148806470+Bishakha143@users.noreply.github.com>
2023-11-05 23:41:58 +0530

    Add files via upload

---
+++ root1.py
@@ -0,0 +1,9 @@
+"""Num= int(input("enter a number here:"))
+sr= Num**(1/2)
+print("The square root of the given number is",sr)
+"""
+
+import math
+num= int(input("Enter a number here:"))
+Sr= math.sqrt(num)
+print("The square root of the given number is",Sr)
